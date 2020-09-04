a3d9e6d139fc682be90a8cf91ce8c0cd69c5d324
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-04 10:56:45 +0000

    Deploy appelmar/gdalcubes to github.com/appelmar/gdalcubes.git:gh-pages

---
+++ navtreedata.js
@@ -0,0 +1,39 @@
+var NAVTREE =
+[
+  [ "gdalcubes", "index.html", [
+    [ "gdalcubes - Earth observation data cubes from GDAL image collections", "index.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Enumerations", "functions_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classgdalcubes_1_1datetime.html#ad88835af3821f19e3015a187dd3a2edc",
+"progress_8h_source.html",
+"structgdalcubes_1_1reducer__singleband.html#a96564a051dd79ad26b43483ce8ece66a"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
